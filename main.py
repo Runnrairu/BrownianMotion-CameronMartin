2fcaab0084d758be0ee4f6ddc507a5f40277b16b
rinnarua <runnrairu@gmail.com>
2025-01-25 14:09:13 +0900

    Update main.py
    取り急ぎコードだけ

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 
 import numpy as np
 import time
-
+rng = np.random.default_rng()
 pi=3.14159265358979323
 
 def basis_CameronMartin(k,x,T):
-    if k==0:
+    if False: #k==0
         return x/np.sqrt(T)
     else:
         return np.sqrt(2*T)*np.sin((k+0.5)*pi*x/T)/(pi*(k+0.5))
@@ -14,29 +14,53 @@ def basis_CameronMartin(k,x,T):
     
 
 
-n=100001 #時間ステップの数 t=0を１つめとする
-m=10 #用いるCameron martin空間の基底
+n=100000 #時間ステップの数 t=0を１つめとする
+m=1000 #用いるCameron martin空間の基底
 T=10.0
 path=10000 #生成したいブラウン運動の数
 delta_t=T/n
 
-CM=[[0]* n  for j in range(m)]
-for i in range(m):
-    for j in range(n):
-        CM[i][j]=basis_CameronMartin(i, delta_t*j,T)
+
+        
+
+
+brownian_normal=[[0 for j in range(n)] for i in range(path)]
+brownian_Cameron=[[0 for j in range(n)] for i in range(path)]
+
 
 
 
 start = time.time() 
-
-
-
-
+noise_normal=rng.standard_normal((path, n-1))
+scale=np.sqrt(T/n)
+for i in range(path):
+    for j in range(n-1):
+        brownian_normal[i][j+1]=brownian_normal[i][j]+scale*noise_normal[i][j]
 
 end = time.time() 
 time_diff = end - start 
 print(time_diff) 
 
+start=time.time() 
+CM=[[0]* n  for j in range(m)]
+for k in range(m):
+    for j in range(n):
+        CM[k][j]=basis_CameronMartin(k, delta_t*j,T)
 
+noise_cameron=rng.standard_normal((path, m))
+brownian_Cameron=np.dot(noise_cameron,CM)
 
+end = time.time() 
+time_diff = end - start 
+print(time_diff) 
+
+mean=np.mean(brownian_Cameron,axis=0)
+variance = np.var(brownian_Cameron, axis=0)
+
+print(mean,variance)
+
+mean=np.mean(brownian_normal,axis=0)
+variance = np.var(brownian_normal, axis=0)
+
+print(mean,variance)
 
